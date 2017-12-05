d8c721382681c103e4240c5ad0ee99a6808879f1
yarind <yarind@tradair.com>
2017-12-05 10:25:44 +0200

    master-todo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,96 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
 
-document.addEventListener('DOMContentLoaded', function() {
-    ReactDOM.render(
-        React.createElement(Counter),
-        document.getElementById('mount')
-    );
-});
+class ContentHeader extends PureComponent {
+
+    inputChanged(event) {
+        event.preventDefault();
+        let text = this.refs.newTodoText.value;
+        if (text) {
+            this.props.createNewTodo(text);
+            this.refs.newTodoText.value = '';
+        }
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.inputChanged.bind(this)}>
+                <input type="text" placeholder="What do you want to do?" ref="newTodoText" />
+                <button type="submit">Submit</button >
+            </form >
+        )
+    }
+}
+
+const TodoRow = ({ content, isChecked }) => (
+    <div >
+        <input type="checkbox" checked={isChecked} onChange={function () {
+            console.log('change!')
+        }} />
+        <span >{content}</span >
+    </div >
+);
+
+const TodosList = ({ todosList }) => (
+    <div >
+        {
+            todosList && todosList.map(function (item, index) {
+                return <TodoRow content={item.content} isChecked={item.isChecked} key={index} />
+            })
+        }
+    </div >
+);
+
+
+class TodoApp extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            todoItems: [],
+        }
+    }
+
+    createNewTodo(text){
+        debugger;
+        this.state.todoItems.push({
+            content: text,
+            isChecked: false,
+        });
+        console.log(this.state);
+        this.setState({todoItems: this.state.todoItems});
+
+    }
+
+    render() {
+        const defaultTodos = [
+            {
+                content: 'yarin needs to learn react',
+                isChecked: false,
+            },
+            {
+                content: 'yarin needs to calm down',
+                isChecked: false,
+            },
+            {
+                content: 'yarin needs to create GOOD toDo list',
+                isChecked: false,
+            },
+            {
+                content: 'yarin needs to schedule interview',
+                isChecked: true,
+            },
+        ];
+        return (
+            <div >
+                <ContentHeader createNewTodo={this.createNewTodo.bind(this)}/>
+                <TodosList todosList={this.state.todoItems.todoItems} />
+                {console.log(this.state.todoItems)}
+            </div >
+        );
+    }
+}
+
+ReactDOM.render(
+    <TodoApp />,
+    document.getElementById('todoApp'),
+);
