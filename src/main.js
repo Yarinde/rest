bba7618a6dd8cef61eb8ea43458778841c5d8c93
yarind <yarind@tradair.com>
2017-12-06 12:36:02 +0200

    master-todo implemented only with react

--- src/main.js
+++ src/main.js
@@ -1,44 +1,80 @@
 import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
+import { Button, Navbar, FormControl, FormGroup } from 'react-bootstrap';
 
 class ContentHeader extends PureComponent {
 
     inputChanged(event) {
         event.preventDefault();
-        let text = this.refs.newTodoText.value;
+        debugger;
+        let text = this.inputNode.value;
         if (text) {
             this.props.createNewTodo(text);
-            this.refs.newTodoText.value = '';
+            this.inputNode.value = '';
         }
     }
 
     render() {
         return (
-            <form onSubmit={this.inputChanged.bind(this)}>
-                <input type="text" placeholder="What do you want to do?" ref="newTodoText" />
-                <button type="submit">Submit</button >
-            </form >
-        )
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">To Do List</a>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <form onSubmit={this.inputChanged.bind(this)}>
+                    <FormGroup>
+                        <FormControl type="text" placeholder="What do you want to do?"
+                                     inputRef={node => this.inputNode = node} />
+                        <Button type="submit">Submit</Button>
+                    </FormGroup>
+                </form>
+            </Navbar>
+        );
     }
 }
 
-const TodoRow = ({ content, isChecked }) => (
-    <div >
-        <input type="checkbox" checked={isChecked} onChange={function () {
-            console.log('change!')
-        }} />
-        <span >{content}</span >
-    </div >
-);
+class TodoRow extends PureComponent {
 
-const TodosList = ({ todosList }) => (
-    <div >
+    onCheckboxChange(event) {
+        const rowId = event.target.attributes.id.value,
+            isChecked = event.target.checked;
+        this.props.onTodoCheck(isChecked, rowId);
+    }
+
+    render() {
+        const { content, isChecked, id } = this.props;
+        return (
+            <div>
+                {
+                    isChecked ? (
+                            <div>
+                                <input type="checkbox" id={id} defaultChecked={isChecked}
+                                       onChange={this.onCheckboxChange.bind(this)} />
+                                <del><span>{content}</span></del>
+                            </div>
+                        ) :
+                        (
+                            <div>
+                                <input type="checkbox" id={id} defaultChecked={isChecked}
+                                       onChange={this.onCheckboxChange.bind(this)} />
+                                <span>{content}</span>
+                            </div>
+                        )
+                }
+            </div>
+        );
+    }
+}
+
+const TodosList = ({ todosList, ...props }) => (
+    <div>
         {
             todosList && todosList.map(function (item, index) {
-                return <TodoRow content={item.content} isChecked={item.isChecked} key={index} />
+                return <TodoRow content={item.content} isChecked={item.isChecked} id={index} key={index} {...props} />;
             })
         }
-    </div >
+    </div>
 );
 
 
@@ -46,46 +82,48 @@ class TodoApp extends PureComponent {
     constructor() {
         super();
         this.state = {
-            todoItems: [],
-        }
+            todoItems: [
+                {
+                    content: 'yarin needs to learn react',
+                    isChecked: false,
+                },
+                {
+                    content: 'yarin needs to calm down',
+                    isChecked: false,
+                },
+                {
+                    content: 'yarin needs to create GOOD toDo list',
+                    isChecked: false,
+                },
+                {
+                    content: 'yarin needs to schedule interview',
+                    isChecked: true,
+                },
+            ],
+        };
     }
 
-    createNewTodo(text){
-        debugger;
-        this.state.todoItems.push({
+    createNewTodo(text) {
+        let todoItems = this.state.todoItems.slice();
+        todoItems.push({
             content: text,
             isChecked: false,
         });
-        console.log(this.state);
-        this.setState({todoItems: this.state.todoItems});
+        this.setState({ todoItems: todoItems });
+    }
 
+    checkTodoRow(isChecked, rowId) {
+        const todoCheckedItems = this.state.todoItems.slice();
+        todoCheckedItems[rowId].isChecked = isChecked;
+        this.setState({ todoItems: todoCheckedItems });
     }
 
     render() {
-        const defaultTodos = [
-            {
-                content: 'yarin needs to learn react',
-                isChecked: false,
-            },
-            {
-                content: 'yarin needs to calm down',
-                isChecked: false,
-            },
-            {
-                content: 'yarin needs to create GOOD toDo list',
-                isChecked: false,
-            },
-            {
-                content: 'yarin needs to schedule interview',
-                isChecked: true,
-            },
-        ];
         return (
-            <div >
-                <ContentHeader createNewTodo={this.createNewTodo.bind(this)}/>
-                <TodosList todosList={this.state.todoItems.todoItems} />
-                {console.log(this.state.todoItems)}
-            </div >
+            <div>
+                <ContentHeader createNewTodo={this.createNewTodo.bind(this)} />
+                <TodosList todosList={this.state.todoItems} onTodoCheck={this.checkTodoRow.bind(this)} />
+            </div>
         );
     }
 }
